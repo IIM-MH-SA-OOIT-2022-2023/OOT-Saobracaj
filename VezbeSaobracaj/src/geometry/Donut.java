5df18d6355740a7ad130a4c8fb4a392765a94f31
Vladimir <Vladimir@DESKTOP-BAQNI43>
2022-11-28 16:42:47 +0100

    Klasa Donut

--- VezbeSaobracaj/src/geometry/Donut.java
+++ VezbeSaobracaj/src/geometry/Donut.java
@@ -19,6 +19,37 @@ public class Donut extends Circle {
 		this.selected = selected;
 	}
 	
+	public double area() {
+		return super.area() - innerRadius*innerRadius*Math.PI;
+	}
+	
+	public boolean contains(int x, int y) {
+		return getCenter().distance(new Point(x,y)) <= getRadius() &&
+				getCenter().distance(new Point(x,y)) >= innerRadius;
+	}
+	
+	public boolean contains(Point p) {
+		return contains(p.getX(), p.getY());
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + ", inner radius: " + innerRadius;
+ 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Donut) {
+			Donut temp = (Donut)obj;
+			if(temp.getCenter().equals(super.getCenter()) &&
+					temp.getRadius() == super.getRadius()
+					&& temp.getInnerRadius() == innerRadius) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	
 	public void setSelected(boolean selected) {
 		this.selected = selected;
